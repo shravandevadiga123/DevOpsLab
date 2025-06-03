f16e8a1e35bef26eefbd5ef192786130871ed6f3
shravandevadiga123 <shravanlaptop06@gmail.com>
2025-06-03 09:10:33 +0530

    change us privilege of db

--- db.js
+++ db.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'host.docker.internal', // For Docker on Windows/macOS
-    user: 'root',
-    password: '', // Leave empty if no password is set
-    database: 'meeting_room_booking',
-    port: 3306,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
